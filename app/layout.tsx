e5dfe7fdc8c0956dbc6b915e52574f36cb989b79
Samuel Pokam <dixiades@gmail.com>
2024-08-10 10:42:57 -0400

    Project setup

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+import type { Metadata } from "next"
+import { Plus_Jakarta_Sans } from "next/font/google"
+import "./globals.css"
+
+const plusJakarta = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "QuickMed",
+  description: "A healthcare platform for everyone",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          plusJakarta.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
